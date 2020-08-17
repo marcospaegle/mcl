5efabe0384afdeff0288c00bb352a6e64aef5cea
Marcos Paegle <marcos.paegle@gmail.com>
2020-08-17 10:38:19 +0100

    add docker-compose file and tweak the home.blade.php

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/marcospaegle/Code/marvel-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/marcospaegle/Code/marvel-app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
